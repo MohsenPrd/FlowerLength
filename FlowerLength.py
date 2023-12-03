aaa5c758b8c4ed6fd134a88568058a8ce530355b
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2023-12-03 20:39:29 +0330

    Create FlowerLength.py
    shows the length of list

---
+++ FlowerLength.py
@@ -0,0 +1,10 @@
+## Mohsen Pourdehghan
+
+flower_list=['Orchid','Iris','Rose','Tulip','Daisy','Lily','Daffodil','Sunflower']
+
+x = 0
+
+for name in flower_list:
+    x = x + 1
+    print(f'flower_{x}: {name}')
+    print(f'length= {len(name)}')
